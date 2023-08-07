d63ba4aea3ed7ca0b8bc5e9f7843713c8bf2dd67
Ishan Shrestha <ishan.shrestha@laliguras.de>
2023-08-07 13:32:04 +0545

    Added questions

--- src/Questions.js
+++ src/Questions.js
@@ -4,12 +4,90 @@ const Questions = {
       questions: [
         {
           index: 1,
-          question: "What is your name ?",
+          question: "What is your name?",
+          type: "open enden",
           answer: ""
         },
         {
           index: 2,
-          question: "What is your age in years ?",
+          question: "What is your age in years?",
+          type: "open enden",
+          answer: ""
+        },
+        {
+          index: 3,
+          question: "Do you want to include a picture of yourself in your application documents?",
+          type: "yes no",
+          answer: ""
+        },
+        {
+          index: 4,
+          question: "Which area(s) do you have the most experience in? Write your own answers or choose one or more of the suggestions below.",
+          answer: ""
+        },
+        {
+          index: 5,
+          question: "What are your main personal qualities?",
+          answer: ""
+        },
+        {
+          index: 6,
+          question: "Enter the phone number you want to be included in your application documents. You don't have to answer this question if you don't want to, but your application documents will be better if you do.",
+          answer: ""
+        },
+        {
+          index: 7,
+          question: "Which city do you work in or want a job in? You don't have to answer this question if you don't want to, but your application documents will be better if you do.",
+          answer: ""
+        },
+        {
+          index: 8,
+          question: "What skills do you want to highlight in your CV?",
+          answer: ""
+        },
+        {
+          index: 9,
+          question: "Which language(s) do you have knowledge of? If you want, you can indicate how good you are in each language in a later question.",
+          answer: ""
+        },
+        {
+          index: 10,
+          question: "Do you want to indicate how good you are in each language?",
+          answer: ""
+        },
+        {
+          index: 11,
+          question: "How are your Arabic skills?",
+          answer: ""
+        },
+        {
+          index: 12,
+          question: "What are your interests?",
+          answer: ""
+        },
+        {
+          index: 13,
+          question: "Which certification(s) do you want to include in your CV? Remember to only include certifications that are relevant to the jobs you are applying for.",
+          answer: ""
+        },
+        {
+          index: 14,
+          question: "What prizes or awards do you want to include in your CV?",
+          answer: ""
+        },
+        {
+          index: 15,
+          question: "Which area(s) do you have the most experience in? Write your own answers or choose one or more of the suggestions below.",
+          answer: ""
+        },
+        {
+          index: 16,
+          question: "What are your main personal qualities?",
+          answer: ""
+        },
+        {
+          index: 17,
+          question: "Do you want a general summary or tailor your summary for a specific job/company? You increase your chances of getting an interview if you tailor your summary to each job you apply for.",
           answer: ""
         }
       ]
